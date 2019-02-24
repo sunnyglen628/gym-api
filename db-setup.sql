4695f0fae63a29bf6ac0fafd6af1d36cb762ce72
sunnyglen628 <34423320+sunnyglen628@users.noreply.github.com>
2019-02-23 19:05:22 -0500

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `medical_history`
 --
 create database gymdb;
-use database gymdb;
+use  gymdb;
 DROP TABLE IF EXISTS `medical_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
